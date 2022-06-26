1e2fa3debb5f17f8762b64dc16254cdce7e82cc3
Yuriy Ivon <YuriyIvon@users.noreply.github.com>
2022-06-26 11:55:22 +0300

    Fixed unit tests

--- tests/DatabaseBenchmark.Tests/Reporting/ResultsBuilderTests.cs
+++ tests/DatabaseBenchmark.Tests/Reporting/ResultsBuilderTests.cs
@@ -12,15 +12,15 @@ namespace DatabaseBenchmark.Tests.Reporting
             string name = "Sample Name";
             var metrics = new MetricsCollection(name);
             var currentTime = DateTime.Now;
-            metrics.Metrics.Add(new Metric(currentTime.AddSeconds(-1), currentTime));
-            metrics.Metrics.Add(new Metric(currentTime.AddSeconds(-2), currentTime));
-            metrics.Metrics.Add(new Metric(currentTime.AddSeconds(-3), currentTime));
+            metrics.Metrics.Add(new Metric(currentTime.AddSeconds(-1), currentTime, 1));
+            metrics.Metrics.Add(new Metric(currentTime.AddSeconds(-2), currentTime, 2));
+            metrics.Metrics.Add(new Metric(currentTime.AddSeconds(-3), currentTime, 3));
             var resultsBuilder = new ResultsBuilder();
 
             var results = resultsBuilder.Build(new[] { metrics });
 
             Assert.Single(results.Rows);
-            Assert.Equal(8, results.Columns.Count);
+            Assert.Equal(9, results.Columns.Count);
             Assert.Equal(name, results.Rows[0]["name"]);
             Assert.Equal(2000.0, results.Rows[0]["avg"]);
             Assert.Equal(1000.0, results.Rows[0]["min"]);
@@ -29,6 +29,7 @@ namespace DatabaseBenchmark.Tests.Reporting
             Assert.Equal(2000.0, results.Rows[0]["p50"]);
             Assert.Equal(2000.0, results.Rows[0]["p90"]);
             Assert.Equal(1.0, results.Rows[0]["qps"]);
+            Assert.Equal(2.0, results.Rows[0]["avgRows"]);
         }
     }
 }
