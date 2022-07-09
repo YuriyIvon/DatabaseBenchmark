69f8f81579359e47454fccfe3da2687a745f5b77
Yuriy Ivon <YuriyIvon@users.noreply.github.com>
2022-07-09 10:52:36 +0300

    Added Oracle query builder unit tests and updated the documentation

---
+++ tests/DatabaseBenchmark.Tests/Databases/OracleQueryBuilderTests.cs
@@ -0,0 +1,47 @@
+﻿using DatabaseBenchmark.Databases.Sql;
+using DatabaseBenchmark.Tests.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DatabaseBenchmark.Tests.Databases
+{
+    public class OracleQueryBuilderTests
+    {
+        [Fact]
+        public void BuildQueryNoArguments()
+        {
+            var parametersBuilder = new SqlParametersBuilder(":");
+            var builder = new SqlQueryBuilder(SampleInputs.Table, SampleInputs.NoArgumentsQuery, parametersBuilder, null);
+
+            var queryText = builder.Build();
+
+            var normalizedQueryText = queryText.NormalizeSpaces();
+            Assert.Equal(@"SELECT * FROM Sample", normalizedQueryText);
+        }
+
+        [Fact]
+        public void BuildQueryAllArguments()
+        {
+            var query = SampleInputs.AllArgumentsQuery;
+            var parametersBuilder = new SqlParametersBuilder(":");
+            var builder = new SqlQueryBuilder(SampleInputs.Table, query, parametersBuilder, null);
+
+            var queryText = builder.Build();
+
+            var normalizedQueryText = queryText.NormalizeSpaces();
+            Assert.Equal("SELECT Category, SubCategory, SUM(Price) TotalPrice FROM Sample"
+                + " WHERE (Category = :p0 AND SubCategory IS NULL)"
+                + " GROUP BY Category, SubCategory"
+                + " ORDER BY Category ASC, SubCategory ASC"
+                + " OFFSET :p1 ROWS FETCH NEXT :p2 ROWS ONLY", normalizedQueryText);
+            Assert.Equal(3, parametersBuilder.Values.Count);
+            Assert.Equal("ABC", parametersBuilder.Values[":p0"]);
+            Assert.Equal(10, parametersBuilder.Values[":p1"]);
+            Assert.Equal(100, parametersBuilder.Values[":p2"]);
+        }
+    }
+}
