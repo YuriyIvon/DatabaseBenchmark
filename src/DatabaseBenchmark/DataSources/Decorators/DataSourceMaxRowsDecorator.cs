5648f1ae799787985aa2bc5b7bc2e7ce128e9abc
Yuriy Ivon <YuriyIvon@users.noreply.github.com>
2023-12-16 10:00:19 +0000

    Added the missing file

---
+++ src/DatabaseBenchmark/DataSources/Decorators/DataSourceMaxRowsDecorator.cs
@@ -0,0 +1,34 @@
+﻿using DatabaseBenchmark.DataSources.Interfaces;
+
+namespace DatabaseBenchmark.DataSources.Decorators
+{
+    public sealed class DataSourceMaxRowsDecorator : IDataSource
+    {
+        private readonly IDataSource _dataSource;
+
+        private int _remainingRows;
+
+        public DataSourceMaxRowsDecorator(IDataSource dataSource, int maxRows)
+        {
+            _dataSource = dataSource;
+            _remainingRows = maxRows;
+        }
+
+        public object GetValue(Type type, string name) => _dataSource.GetValue(type, name);
+
+        public bool Read()
+        {
+            if (_remainingRows > 0)
+            {
+                _remainingRows--;
+                return _dataSource.Read();
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void Dispose() => _dataSource.Dispose();
+    }
+}
