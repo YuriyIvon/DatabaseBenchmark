32825dd3a463f721df3af4990cc5c2ab9f7c3d5d
Yuriy Ivon <YuriyIvon@users.noreply.github.com>
2024-10-20 09:17:35 +0100

    Added the missing file

---
+++ src/DatabaseBenchmark/Databases/ClickHouse/ClickHouseNoParametersBuilder.cs
@@ -0,0 +1,16 @@
+﻿using DatabaseBenchmark.Common;
+using DatabaseBenchmark.Databases.Sql;
+using DatabaseBenchmark.Model;
+
+namespace DatabaseBenchmark.Databases.ClickHouse
+{
+    public class ClickHouseNoParametersBuilder : SqlNoParametersBuilder
+    {
+        public override string Append(object value, ColumnType type, bool array) =>
+            value switch
+            {
+                IEnumerable<object> arrayValue => $"[{string.Join(", ", arrayValue.Select(x => Append(x, type, false)))}]",
+                _ => base.Append(value, type, array)
+            };
+    }
+}
