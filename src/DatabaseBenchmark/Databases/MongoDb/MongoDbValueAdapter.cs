23e83992459ec3d6842e7fdba0d5730858cf3a60
Yuriy Ivon <YuriyIvon@users.noreply.github.com>
2025-03-09 11:00:56 +0000

    Added missing file

---
+++ src/DatabaseBenchmark/Databases/MongoDb/MongoDbValueAdapter.cs
@@ -0,0 +1,16 @@
+﻿using DatabaseBenchmark.Model;
+using MongoDB.Bson;
+
+namespace DatabaseBenchmark.Databases.MongoDb
+{
+    public static class MongoDbValueAdapter
+    {
+        public static BsonValue CreateBsonValue(ColumnType columnType, object value) =>
+            value switch
+            {
+                Guid guidValue when columnType == ColumnType.Guid => new BsonBinaryData(guidValue, GuidRepresentation.Standard),
+                string stringValue when columnType == ColumnType.Guid => new BsonBinaryData(new Guid(stringValue), GuidRepresentation.Standard),
+                _ => BsonValue.Create(value)
+            };
+    }
+}
