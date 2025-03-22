2c663a02b998cc49520ee4cca5aba60b1c963851
Yuriy Ivon <YuriyIvon@users.noreply.github.com>
2025-03-22 23:32:30 +0000

    Fixed an issue with decimal values formatting

--- src/DatabaseBenchmark/Common/ObjectExtensions.cs
+++ src/DatabaseBenchmark/Common/ObjectExtensions.cs
@@ -2,6 +2,6 @@
 {
     public static class ObjectExtensions
     {
-        public static bool IsNumber(this object value) => value is byte or short or ushort or int or uint or long or ulong or double or float;
+        public static bool IsNumber(this object value) => value is byte or short or ushort or int or uint or long or ulong or double or float or decimal;
     }
 }
